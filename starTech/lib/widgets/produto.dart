c057170696379b3c68ba69d6ebca514c8e05ec90
Sofia <sofiagmansur@icloud.com>
2023-08-29 23:11:46 -0300

    Container dos produtos

--- starTech/lib/widgets/produto.dart
+++ starTech/lib/widgets/produto.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -14,15 +16,42 @@ class ProdutoState extends State {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: <Widget>[
-          Container(
-            height: 30,
-            width: 30,
-            decoration: const BoxDecoration(
-              color:  Color.fromARGB(255, 154, 42, 164),
-            ),
-          )
-        ],
-      );
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              color: Colors.pink[100], borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            children: [
+              Container(
+                height: 200.0,
+                 width: 200.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/imagem1.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Container(
+                child: const Text("Teste"),
+              ),
+              Container(
+                height: 15.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.pink[800],
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Text("R\$ 1000,00"), 
+                )
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
