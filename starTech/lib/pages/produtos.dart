1c7dc2242c7160b66c3be80b6d5406c4b8092798
Sofia <sofiagmansur@icloud.com>
2023-08-31 00:34:22 -0300

    Melhorias e arrumacoes

--- starTech/lib/pages/produtos.dart
+++ starTech/lib/pages/produtos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hackadev/widgets/recomendados.dart';
 import 'package:hackadev/widgets/search_bar.dart';
+import 'package:hackadev/widgets/recomendados.dart';
+import 'package:hackadev/widgets/categorias.dart';
+import 'package:hackadev/widgets/destaques.dart';
 
 class Produtos extends StatefulWidget {
   const Produtos({super.key});
@@ -14,25 +16,16 @@ class Produtos extends StatefulWidget {
 class ProdutosState extends State {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        const AppSearchBar(),
-        Flexible(
-            child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: const <Widget>[
-            Recomendados(),
-            Recomendados(),
-            Recomendados(),
-            Recomendados(),
-            
-          ],
-        ))
-      ],
-    );
+   return const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Estende as seções horizontalmente
+        children: [
+          AppSearchBar(),
+          Categorias(),
+          SizedBox(height: 20), // Espaçamento entre as seções
+          Destaques(),
+          SizedBox(height: 30),
+          Flexible(child: Recomendados()),
+        ],
+      );
   }
 }
