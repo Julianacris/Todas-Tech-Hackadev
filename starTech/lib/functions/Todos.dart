596a0c6355248d47cbb4ad7c273b8a08845b8613
Sofia <sofiagmansur@icloud.com>
2023-09-01 01:11:27 -0300

    implementacao dos produtos

--- starTech/lib/functions/Todos.dart
+++ starTech/lib/functions/Todos.dart
@@ -25,31 +25,31 @@ class TodosProdState extends State {
       crossAxisCount: 2,
       children: const <Widget>[
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone 14 Azul",
+            preco: ' R\$ 4.249,00',
+            nome: "Iphone 13 128gb Azul",
             pathImagem: 'assets/images/iphone-azul.png'),
         Produto(
             preco: ' R\$ 869,90',
             nome: "Filtro de água Consul",
             pathImagem: 'assets/images/filtro-agua.webp'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone 14 Amarelo",
+            preco: ' R\$ 4.249,00',
+            nome: "Iphone 13 128gb Amarelo",
             pathImagem: 'assets/images/iphone-amarelo.png'),
         Produto(
             preco: ' R\$ 429,00',
             nome: "Alexa Echo Dot",
             pathImagem: 'assets/images/alexa-echo.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone 14 Branco",
+            preco: ' R\$ 3.698,99',
+            nome: "Iphone 12 64gb",
             pathImagem: 'assets/images/iphone-branco.png'),
         Produto(
             preco: ' R\$ 3.858,57',
             nome: "Smartphone Motorola Edge 30 Ultra",
             pathImagem: 'assets/images/celular-motorola.png'),
         Produto(
-            preco: ' R\$ 3.779,10.',
+            preco: ' R\$ 3.779,10',
             nome: "Samsung Galaxy S22 Ultra",
             pathImagem: 'assets/images/celular-samsung-s22.png'),
         Produto(
@@ -61,44 +61,44 @@ class TodosProdState extends State {
             nome: "Drone Dji Mini 3 Pro",
             pathImagem: 'assets/images/drone-dji.png'),
         Produto(
-            preco: ' R\$ 20,00',
-            nome: "Teste",
+            preco: ' R\$ 1.069,00',
+            nome: "Purificador De Água Electrolux",
             pathImagem: 'assets/images/purificador-agua.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone1",
+            preco: ' R\$ 399,00',
+            nome: "Fone De Ouvido Jbl Tune",
             pathImagem: 'assets/images/Headphone-Tune-JBL.png'),
         Produto(
-            preco: ' R\$ 20,00',
-            nome: "Teste",
-            pathImagem: 'assets/images/relogoi-amazfit.png'),
+            preco: ' R\$ 552,92',
+            nome: "Smartwatch Amazfit Gts 4 Mini",
+            pathImagem: 'assets/images/relogio-amazfit.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone1",
+            preco: ' R\$ 538,90',
+            nome: "Robô Aspirador Mars",
             pathImagem: 'assets/images/robo-aspirador.png'),
         Produto(
-            preco: ' R\$ 20,00',
-            nome: "Teste",
+            preco: ' R\$ 2.849,00',
+            nome: "Smart Tv LG 55pol Led 4K UHD",
             pathImagem: 'assets/images/tv-Lg.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone1",
+            preco: ' R\$ 4.986,00',
+            nome: "Smart Tv Samsung 55pol Neo Qled",
             pathImagem: 'assets/images/tv-samsung-neo.png'),
         Produto(
-            preco: ' R\$ 20,00',
-            nome: "Teste",
+            preco: ' R\$ 5.823,90',
+            nome: "Smart Tv 65pol Tcl Qled",
             pathImagem: 'assets/images/tv-tcl.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone1",
+            preco: ' R\$ 1.089,00',
+            nome: "Smart Tv Monitor 24pol Led",
             pathImagem: 'assets/images/tvSmart-Lg.png'),
         Produto(
-            preco: ' R\$ 20,00',
-            nome: "Teste",
+            preco: ' R\$ 2.089,00',
+            nome: "Tv Philips 50pol Smart 4K",
             pathImagem: 'assets/images/tvSmart-philips.png'),
         Produto(
-            preco: ' R\$ 10,00',
-            nome: "iPhone1",
+            preco: ' R\$ 1.399,00',
+            nome: "Smart Tv 40pol Philco Led",
             pathImagem: 'assets/images/tvSmart-philco.png'),
       ],
     );
